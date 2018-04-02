7c9273ee34c75ca94994d3f0155c75d22228b198
YI-HAN CHEN <hannah51125@gmail.com>
2018-04-02 18:12:58 +0800

    find top news

--- scraper.py
+++ scraper.py
@@ -16,7 +16,7 @@ for link in soupObj.find_all('a'):
     if u'運動' in s:
         temp = link.get('href')
         sports = temp
-        print("sports" + temp)
+        print("sports >>>>>" + temp)
         print("link >>>>>> " + base_url+temp)
         break
 
@@ -26,16 +26,23 @@ req2 = requests.get(sports_url)
 print(req2.status_code)
 soupObj2 = BeautifulSoup(req2.text, 'html.parser')
 
-for dl in soupObj2.find_all("dl", attrs={"class": "listing"} ):
-    print(dl.find_next("dt").find_all_next("h2"))
-
+# for dl in soupObj2.find_all("dl", attrs={"class": "listing"} ):
+#     print(dl.find_next("dt").find_all_next("h2"))
+print("==================")
 ob = soupObj2.find_all("div", attrs={'class': 'area'})
 for i in ob:
     x = i.find_next("h3")
-    # print(x.contents)
+    print(x.contents)
     if u"最新文章" in x.contents:
-        print("there") 
-        t = x.find_all_next("dl")
-        print(t)
+        # print("there") 
+        print("==================")
+        top_news = x.next_sibling
+        for item in top_news.find_all("h2"):
+            print(item.get_text())
+        # t = x.find_next("dl", attrs={"class": "listing"} )
+        
+        # for child in t.descendants:
+        #     print(child)
+            # print(t)
         break
 
