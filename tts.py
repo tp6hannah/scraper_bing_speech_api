fd162146277d2e2013eb228fe2f948ff7400cbe1
YI-HAN CHEN <hannah51125@gmail.com>
2018-04-02 19:02:07 +0800

    use pyaudio play sound

--- tts.py
+++ tts.py
@@ -8,9 +8,12 @@ Bing speech document: https://docs.microsoft.com/en-us/azure/cognitive-services/
 import requests as req
 import os
 from xml.etree import ElementTree
+import pyaudio
+import wave
+import sys
 
 # Access the token from api_key 
-api_key = 'None'
+api_key = ''
 headers = {'Ocp-Apim-Subscription-Key': api_key}
 get_token = req.session()
 access_token = get_token.post('https://api.cognitive.microsoft.com/sts/v1.0/issueToken', headers=headers).text
@@ -25,7 +28,7 @@ voice = ElementTree.SubElement(body, 'voice')
 voice.set('xml:lang', 'en-us')
 voice.set('xml:gender', 'Female')
 voice.set('name', 'Microsoft Server Speech Text to Speech Voice (zh-TW, Yating, Apollo)')
-voice.text = "今天天氣很好，宗毅不開心，因為他在準備教案不能出去玩"
+voice.text = "今天天氣很好"
 # 建立資料夾
 
 get_record = req.session()
@@ -35,3 +38,31 @@ respond = get_record.post('https://speech.platform.bing.com/synthesize', data=El
 os.makedirs('./sounds', exist_ok=True)
 with open('./sounds/' + 'test' + '.wav', 'wb') as f:
     f.write(respond.content)
+
+
+
+
+# play the sound we just turn in
+
+CHUNK = 1024
+
+wf = wave.open("./sounds/test.wav", 'rb')
+
+p = pyaudio.PyAudio()
+
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+data = wf.readframes(CHUNK)
+
+while len(data) > 0:
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
+
